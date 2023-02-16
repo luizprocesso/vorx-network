eca7270f7542e60bfb2ff0b1defd225ef9325162
root <root@ip-172-31-4-252.ec2.internal>
2023-02-16 00:34:37 +0000

    subnets na az b

--- network.tf
+++ network.tf
@@ -26,6 +26,24 @@ resource "aws_subnet" "vorx-subnet-priv-1a" {
   }
 }
 
+resource "aws_subnet" "vorx-subnet-pub-1b" {
+  vpc_id     = aws_vpc.vorx-vpc-prod.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "prod-public-subnet-1b"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-priv-1b" {
+  vpc_id     = aws_vpc.vorx-vpc-prod.id
+  cidr_block = "10.0.20.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "prod-private-subnet-1b"
+  }
+}
+
 ## Outputs ##
 
 output "vpc_vorx_prod_id" {
